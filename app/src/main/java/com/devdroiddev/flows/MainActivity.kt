c2c890d3494cf863cb843bee8b25428e27f59d8a
Talha Waris <talhawaris39@gmail.com>
2023-11-24 15:40:31 +0500

    Events in Flows

--- app/src/main/java/com/devdroiddev/flows/MainActivity.kt
+++ app/src/main/java/com/devdroiddev/flows/MainActivity.kt
@@ -10,16 +10,21 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.util.concurrent.Flow
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+
     companion object {
         const val APP_TAG = "Flows_App"
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -35,41 +40,42 @@ class MainActivity : AppCompatActivity() {
     private fun consumer() {
 
         //  Consumer - 1
-        GlobalScope.launch {
-            val data = producer()
-            data.collect{
-                // Clear the previous text
-                withContext(Dispatchers.Main) {
-          /*          binding.countTv.text = ""
-                    binding.countTv.text = it.toString()*/
+        GlobalScope.launch(Dispatchers.Main) {
+            producer()
+                // Events
+                .onStart {
+                    Log.d(APP_TAG, "Events -> Starting out")
+                    binding.countTv.text = ""
+                    binding.countTv.text = "Starting out"
+                }
+                .onCompletion {
+                    Log.d(APP_TAG, "Events -> Completed")
+                    binding.countTv.text = ""
+                    binding.countTv.text = "Completed"
+                }
+                .onEach {
+                    Log.d(APP_TAG, "Events -> About to emit - $it")
+                    binding.countTv.text = ""
+                    binding.countTv.text = "About to emit"
+                    delay(1000)
+                }
+                .collect {
+                    // Clear the previous text
+                    binding.countTv.text = ""
+                    binding.countTv.text = it.toString()
+                    delay(5000)
                     Log.d(APP_TAG, "Consumer 1 -> ${it.toString()}")
                 }
-            }
         }
 
-        // Consumer - 2
-        GlobalScope.launch {
-            val data = producer()
-            delay(2000)
-            data.collect{
-                // Clear the previous text
-                withContext(Dispatchers.Main) {
-              /*      binding.countTv.text = ""
-                    binding.countTv.text = it.toString()*/
-                    Log.d(APP_TAG, "Consumer 2 -> ${it.toString()}")
-                }
-
-            }
-        }
     }
 
-    private fun producer() = flow<Int>{
+    private fun producer() = flow<Int> {
         val list = listOf<Int>(1, 2, 3, 4, 5)
-        list.forEach{
+        list.forEach {
             delay(1000)
             emit(it)
         }
     }
-
 
 }
